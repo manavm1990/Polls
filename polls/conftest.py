ff5bf216dae604f8adffc2fa7a1924fa4a34c3fc
Manav Misra <manavm@visionwebsoft.com>
2023-03-28 15:53:20 -0500

    :tada: Initial commit

---
+++ polls/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from polls.users.models import User
+from polls.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
