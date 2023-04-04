a234fe4fa33c359936db0198bb7251fcf11fdf53
Manav Misra <manavm@visionwebsoft.com>
2023-04-04 15:22:51 -0500

    test: test that 'pub_date' is read-only

--- polls/questions/tests.py
+++ polls/questions/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from .models import Question
+
+
+@pytest.mark.django_db
+@pytest.fixture
+def question(db):
+    question = Question.objects.create(question_text="What's up?")
+    question.choices.create(choice_text="Not much", votes=0)
+    question.choices.create(choice_text="The sky", votes=0)
+    return question
+
+
+def test_pub_date_is_read_only(question):
+    question.pub_date = "2020-01-01"
+    question.save()
+    assert question.pub_date != "2020-01-01"
